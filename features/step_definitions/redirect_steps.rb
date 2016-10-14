fd8844b930bdd6780b601bf09e0c3886c35a9df1
Pojostick <j.joseph.chen@gmail.com>
2016-10-13 20:08:40 -0700

    Collapsed a step definition

--- features/step_definitions/redirect_steps.rb
+++ features/step_definitions/redirect_steps.rb
@@ -1,6 +1,3 @@
-And /^I have taken the survey before$/ do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-And /^I have not taken the survey before$/ do
+And /^I have (not)? taken the survey before$/ do |not|
   pending # Write code here that turns the phrase above into concrete actions
 end
