76089e2e556385f6f280ba2cce61182accad8671
Muluken Taye <muluken@berkeley.edu>
2016-10-13 21:31:54 -0700

    Features mulukenh (#3)
    * adding downloading feature scenario
    
    * adding downloading features and step definitions
    
    * adding downloading features and step definitions
    
    * deleted readme file added

---
+++ features/step_definitions/download_steps.rb
@@ -0,0 +1,21 @@
+
+Given /the following images in image folder/ do |img|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^I click download on "(.*)"$/ do |img_name|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^I fill in "(.*)" and press "(.*)"$/ do |name, button|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then /^I should get "(.*)"$/ do |file_name| 
+    pending # Write code here that turns the phrase above into concrete actions
+    # "You have already finished the game once. Future results won't be recorded."
+end
+
+Then /^I should be on the same page$/ do 
+    pending # Write code here that turns the phrase above into concrete actions
+end
