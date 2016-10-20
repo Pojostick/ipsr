69e99c66158fce3e5f0cd1eca476a5c381befe06
tusing <tusharsingal@gmail.com>
2016-10-20 03:37:52 +0000

    Configured simplecov to run for both rspec and cucumber using universal config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,4 @@
 require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/config/'
-  add_filter '/lib/'
-  add_filter '/vendor/'
-  add_filter '/features/'
-  
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Views', 'app/views'
-end if not ENV["IGNORE_SIMPLECOV"]
-# run with $ IGNORE_SIMPLECOV=true bundle exec rake spec
 
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
