a65f96079868291d186ec6d354028d1d12864283
Anthony Lai <a.lai@berkeley.edu>
2016-10-19 20:59:09 -0700

    Started controller rspec (#10)
    * started Mosiacs Controller [SKELETON]

---
+++ spec/controllers/mosaics_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe MosaicsController, type: :controller do
+    it "should autosave every 5 seconds" do
+      #setup a mock with no colors
+      #autosave
+      #add in a new tile
+      #autosave
+      #there should be a new tile
+    end
+end
