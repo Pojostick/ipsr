fa5d91b2846051f017e16722f4e1032aa6daa6ff
Pojostick <j.joseph.chen@gmail.com>
2016-10-19 19:21:25 -0700

    Revert "Features d3167502"

--- features/step_definitions/grid_steps.rb
+++ features/step_definitions/grid_steps.rb
@@ -1,11 +1,11 @@
 Then(/^I should see an? (\d+) by (\d+) grid$/) do |arg1, arg2|
+  # check if the # of columns is 8
+  page.all('table#mosaic tr').count.should == arg1
   # check if the # of rows is 10
-  page.all('#mosaic tr').count.should == arg2.to_f
-  # check if the # of rows is 8
-  page.all('#mosaic td').count.should == arg2.to_f * arg1.to_f
+  page.all('table#mosaic td').count.should == arg2*arg1
 end
 Then(/^I should see (\d+) colors$/) do |arg1|
-  page.all('#selection_colors td').count.should == arg1.to_f
+  pending # Write code here that turns the phrase above into concrete actions
 end
 Given(/^the (.*) color in the palette is color "([^"]*)"$/) do |which, color|
   pending # Write code here that turns the phrase above into concrete actions
