bf889e2c36eb069778499b13096b6821ef83af5f
Tushar Singal <tusharsingal@gmail.com>
2016-11-02 23:34:23 -0700

    Revert "Added functionality to mosaic controller to allow for querying of steps" (#26)

--- features/step_definitions/download_steps.rb
+++ features/step_definitions/download_steps.rb
@@ -1,8 +1,7 @@
 Given(/^the following images exist:$/) do |table|
-    pending
-    # table.hashes.each do |mosaic|
-    #     Mosaic.create(mosaic)
-    # end
+    table.hashes.each do |mosaic|
+        Mosaic.create(mosaic)
+    end
   # table is a Cucumber::MultilineArgument::DataTable
 end
 When (/^I follow the link mosaic(.*)$/) do |mosaic_id|
