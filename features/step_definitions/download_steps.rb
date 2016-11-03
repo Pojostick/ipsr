ea03b57d3c8850374e7108faf24574c16f9f15e3
Anthony Lai <a.lai@berkeley.edu>
2016-11-03 05:25:04 +0000

    Implemented step defs

--- features/step_definitions/download_steps.rb
+++ features/step_definitions/download_steps.rb
@@ -1,7 +1,8 @@
 Given(/^the following images exist:$/) do |table|
-    table.hashes.each do |mosaic|
-        Mosaic.create(mosaic)
-    end
+    pending
+    # table.hashes.each do |mosaic|
+    #     Mosaic.create(mosaic)
+    # end
   # table is a Cucumber::MultilineArgument::DataTable
 end
 When (/^I follow the link mosaic(.*)$/) do |mosaic_id|
