5b5799758e4a2568c9f828804967331a4d4bc448
Anthony Lai <a.lai@berkeley.edu>
2016-11-06 23:28:50 +0000

    commented out more pending tests

--- features/step_definitions/download_steps.rb
+++ features/step_definitions/download_steps.rb
@@ -1,10 +1,10 @@
-Given(/^the following images exist:$/) do |table|
-    pending
-    # table.hashes.each do |mosaic|
-    #     Mosaic.create(mosaic)
-    # end
-  # table is a Cucumber::MultilineArgument::DataTable
-end
+# Given(/^the following images exist:$/) do |table|
+#     pending
+#     # table.hashes.each do |mosaic|
+#     #     Mosaic.create(mosaic)
+#     # end
+#   # table is a Cucumber::MultilineArgument::DataTable
+# end
 When (/^I follow the link mosaic(.*)$/) do |mosaic_id|
     click_link("Mosaic ##{mosaic_id}")
 end
