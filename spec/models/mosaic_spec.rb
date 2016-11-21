f1238fc04f12e95cab95a5c00746f2af85708b1b
d3167502 <tommychengtian@hotmail.com>
2016-11-21 00:14:50 -0800

    Features d3167502 (#40)
    * double login feature added
    
    * double login feature added with checking cookies
    
    * Survey and grid features added.
    
    * survey_redirect.feature completed (#1)
    
    * survey_redirect.feature completed
    
    * Update survey_redirect.feature
    
    * Update survey_redirect.feature
    
    * stuff
    
    * Collapsed a step definition
    
    * Typo
    
    * Features mulukenh (#3)
    
    * adding downloading feature scenario
    
    * adding downloading features and step definitions
    
    * adding downloading features and step definitions
    
    * deleted readme file added
    
    * added gallery display features and step definitions (#4)
    
    * Codeclimate filters (#5)
    
    * Preliminary Cucumber features added.
    
    Added step definitions
    
    Fixed ambiguous cucumber step
    
    Updated .gitignore
    
    * Update survey_see_questions.feature
    
    * Added codeclimate coverage reports
    
    * revert schema
    
    * modify double login feature
    
    * Implemented all functionality! yay mosaic test works now (#6)
    
    * Added Mosaic Model and Controller
    
    * index.html completed
    
    * Webpage displays correctly, lots of changes
    
    * Fixed html element generation in haml file (#8)
    
    * Fixed some display issues making extra html elements
    
    * rspec gem stuff added (#9)
    
    * rspec for model done, added gem
    
    * Coverage reports ignore features
    
    * Selection block ids now consistent
    
    * Started controller rspec (#10)
    
    * started Mosiacs Controller [SKELETON]
    
    * Features jasonjaywu (#14)
    
    * added gallery display features and step definitions
    
    * finished cucumber tests
    
    * Features tusing pojostick (#15)
    
    * Codecov
    
    * Features tusing pojostick (#16)
    
    * Codeclimate fix.
    
    * Configured simplecov to run for both rspec and cucumber using universal config file
    
    * Precompiled assets for heroku
    
    * Features mulukenh (#17)
    
    * adding downloading feature scenario
    
    * adding downloading features and step definitions
    
    * adding downloading features and step definitions
    
    * deleted readme file added
    
    * edited step definitions
    
    * rspec test autotest
    
    * solving merging conflict and add rspec
    
    * factories deleted
    
    * resolved conflicting step definitions: display & download
    
    * completed few cucumber scenarios
    
    * Features mulukenh (#18)
    
    * completed few cucumber scenarios
    
    * completed cucumber tests - dowload_steps.rb
    
    * edited gallery view and download_features
    
    * Precompiled assets
    
    * check with master
    
    * check with master
    
    * changed session expiration time and moved double_login feature to feature folder
    
    * add checkable and pagination for gallery
    
    * delete requir_tree in allpliction.js
    
    * ssss
    
    * ssss
    
    * download all
    
    * finished download feature and tests, test coverage is 95%
    
    * update rspec coverage to 95%

--- spec/models/mosaic_spec.rb
+++ spec/models/mosaic_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Mosaic, type: :model do
     it "should have 22 colors" do
       expect(Mosaic.colors).to have(22).items
     end
+    
+    it "should have to_csv method" do
+      expect(Mosaic.to_csv).to have(11).items
+    end
+    
 end
