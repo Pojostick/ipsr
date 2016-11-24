228863c48591536e1a9e150d61404b4703cdb6ce
Anthony Lai <a.lai@berkeley.edu>
2016-11-23 20:15:28 -0800

    Features tony (#46)
    * survey_redirect.feature completed
    
    * Update survey_redirect.feature
    
    * Update survey_redirect.feature
    
    * stuff
    
    * Collapsed a step definition
    
    * Typo
    
    * update tony to match master
    
    * rspec for model done, added gem
    
    * started Mosiacs Controller
    
    * Implemented step defs
    
    * fixed rspec
    
    * commented out all pending tests
    
    * Implemented step defs
    
    * fixed rspec
    
    * commented out all pending tests
    
    * blank commit
    
    * commented out more pending tests
    
    * gridarray 5
    
    * added grids
    
    * oauth functional
    
    * login page
    
    * one liner
    
    * s
    
    * cucumber passing
    
    * #notmycode
    
    * reset key
    
    * resolved merge conflict
    
    * created cucumbers for gallery

---
+++ features/step_definitions/oauth.rb
@@ -0,0 +1,19 @@
+Given(/^that I am not logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the login page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^then if I log in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be able to see the main page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see no image that belongs to someone else$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
